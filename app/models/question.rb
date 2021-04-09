5ee165fdb2ca1695dadef4d2ff1aa2996d5c7780
bsylla <niamatosylla@gmail.com>
2021-04-09 21:48:01 +0200

    Integrate turbo stream for listing questions;

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :session
+  after_create_commit { broadcast_prepend_to "questions" }
+  after_update_commit { broadcast_replace_to "questions" }
+  after_destroy_commit { broadcast_remove_to "questions" }
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,4 @@
 class Session < ApplicationRecord
   has_rich_text :content
+  has_many :questions
 end
