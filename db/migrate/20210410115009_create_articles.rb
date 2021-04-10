7949130cc1795af7701ef0b67af3291a97f737d0
bsylla <niamatosylla@gmail.com>
2021-04-10 13:53:15 +0200

    Add article & category models

---
+++ db/migrate/20210410115009_create_articles.rb
@@ -0,0 +1,10 @@
+class CreateArticles < ActiveRecord::Migration[6.1]
+  def change
+    create_table :articles do |t|
+      t.string :name
+      t.references :category, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
