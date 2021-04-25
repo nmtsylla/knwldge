9f59627c4389e8af1856fa3fbabc310492dab800
bsylla <niamatosylla@gmail.com>
2021-04-25 13:48:30 +0200

    add rspec config and factories

---
+++ spec/factories.rb
@@ -0,0 +1,19 @@
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.email }
+    password { "testtest" }
+    password_confirmation { "testtest" }
+  end
+
+  factory :session do
+    name { Faker::Name.name }
+    startedAt { Faker::Date.in_date_period(month: 2) }
+    user
+  end
+
+  factory :question do
+    content { Faker::Lorem.sentence }
+    session
+    user
+  end
+end
